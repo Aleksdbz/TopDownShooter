02226a88412b5c4cef40c7087ce92b00a5f577a7
Aleksandar Ivic <125746113+Aleksdbz@users.noreply.github.com>
2024-05-21 20:42:00 +0200

    Gravity and Aim log
    added gravity cleaned up a little and added visible aim

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +10,14 @@ public class PlayerMovement : MonoBehaviour
   [Header("Movement Info")]
   [SerializeField]  private Vector3 movementdirection;
   [SerializeField] private float walkspeed;
+
+  private float verticalvelocity;
+  private float gravityscale = 9.81f;
+
+  [Header("Aim Info")]
+  [SerializeField] private Transform aim;
+  [SerializeField] private LayerMask aimLayerMask;
+  private Vector3 lookingDirection;
 
   public Vector2 moveInput;
   public Vector2 aimInput;
@@ -34,16 +40,43 @@ public class PlayerMovement : MonoBehaviour
   private void Update()
   {
     ApplyMovement();
+    AimTowardsMouse();
+  }
+
+  private void AimTowardsMouse()
+  {
+    Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+    if (!Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask)) return;
+    lookingDirection = hitInfo.point - transform.position;
+    lookingDirection.y = 0f;
+    lookingDirection.Normalize();
+
+    transform.forward = lookingDirection;
+
+    aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
   }
 
   private void ApplyMovement()
   {
     movementdirection = new Vector3(moveInput.x, 0, moveInput.y);
+    ApplyGravity();
 
     if (movementdirection.magnitude > 0)
     {
       CharacterController.Move(movementdirection * (Time.deltaTime * walkspeed));
     }
+  }
+
+  private void ApplyGravity()
+  {
+    if (!CharacterController.isGrounded)
+    {
+      verticalvelocity += -gravityscale * Time.deltaTime;
+      movementdirection.y = verticalvelocity;
+    }
+    else
+      verticalvelocity = -.5f;
   }
 
 
